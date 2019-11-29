9dc0101ea2f0cb48a01a6a7ecd86a1caa90e9377
Yunus Emre <yek.plus.plus@gmail.com>
2019-11-29 19:19:16 +0300

    added rest system

---
+++ src/api/rest.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost/',
+  timeout: 5000,
+});
+instance.interceptors.response.use(
+  (response) => (response.data),
+  (error) => {
+    const code = error.response && error.response.data;
+    return (code);
+  },
+);
+export default async (path, body) => {
+  const response = await instance.post(path, body);
+  return response;
+};
